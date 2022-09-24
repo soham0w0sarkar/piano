ec7d0a3a5713c3b638c99810fb6a8f5a99135da7
Soham Sarkar <sohamsarkar59@gmail.com>
2022-09-24 12:51:23 +0530

    script

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
+
 const keys=document.querySelectorAll('.key');
 
 keys.forEach(key => {
   key.addEventListener('click', () => playNote(key))
 })
 
-document.addEventListener('keydown', e => {
-  if (e.repeat) return
-  const key = e.key
-  const whiteKeyIndex = WHITE_KEYS.indexOf(key)
-  const blackKeyIndex = BLACK_KEYS.indexOf(key)
 
-  if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex])
-  if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex])
-})
 
 function playNote(key) {
   const noteAudio = document.getElementById(key.dataset.note)
