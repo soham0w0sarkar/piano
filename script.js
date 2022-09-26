42dae435c9e935b1c217c86330ad0977e0a1e862
Soham Sarkar <sohamsarkar59@gmail.com>
2022-09-26 17:33:46 +0530

    keyboad

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ keys.forEach(key => {
   key.addEventListener('click', () => playNote(key))
 })
 
-<<<<<<< HEAD
+
 document.addEventListener('keydown',event=>{
   const key= event.key;
   const windex = white_key.indexOf(key);
@@ -13,22 +13,19 @@ document.addEventListener('keydown',event=>{
   if(windex>-1) playNote(whites[windex]);
   if(bindex>-1) playNote(blacks[bindex]);
 })
-=======
 
->>>>>>> 55967b33645fcd197fe74a2d6fd6ecb6ccb8848d
+
 
 function playNote(key) {
   const noteAudio = document.getElementById(key.dataset.note)
 
   noteAudio.play()
   noteAudio.currentTime=0;
-<<<<<<< HEAD
+
   noteAudio.play()
   key.classList.add("active");
   noteAudio.addEventListener('ended',()=>{
     key.classList.remove("active");
   })
 }
-=======
-}
->>>>>>> 55967b33645fcd197fe74a2d6fd6ecb6ccb8848d
+
